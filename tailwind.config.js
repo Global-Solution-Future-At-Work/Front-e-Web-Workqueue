28437da35e187c5b3bcefbb5a18963c8b2628251
AzorTartuce <azorzinhopera@gmail.com>
2025-11-12 22:46:39 -0300

    primeiro commit do projeto

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    // Adicione os caminhos para todos os seus arquivos .html, .js, .jsx, etc.
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
